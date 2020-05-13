bba10523bc3fefba49943044a2fe8e497f2f2c4b
Clost <i@clost.net>
2020-05-13 22:51:31 +0800

    增加编辑文章按钮

--- post.php
+++ post.php
@@ -1,4 +1,6 @@
 <?php if($this->user->hasLogin()):?>
+&nbsp; | &nbsp;
+<a href="<?php $this->options->adminUrl(); ?>write-post.php?cid=<?php echo $this->cid;?>" target="_blank">编辑文章</a>
 <?php Typecho_Widget::widget('Widget_Security')->to($security); ?>
 &nbsp; | &nbsp;
 <a href="<?php $security->index('/action/contents-post-edit?do=delete&cid='.$this->cid); ?>" onclick="javascript:return p_del()">删除文章</a>
